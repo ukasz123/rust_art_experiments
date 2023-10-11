c90de6b2fa3380c6113a81da5cd7ef10e4949a11
Łukasz Huculak <ukasz123@gmail.com>
2023-10-11 19:20:52 +0200

    Update workflow and adjust art6-flowers

--- art6-flowers/src/sketch.rs
+++ art6-flowers/src/sketch.rs
@@ -1,10 +1,18 @@
-use nannou::color::{IntoLinSrgba, Alpha};
+use nannou::color::{Alpha, IntoLinSrgba};
 use nannou::draw::primitive::path::DrawingPath;
 use nannou::draw::properties::ColorScalar;
 use nannou::draw::Drawing;
 use nannou::prelude::*;
 use nannou::wgpu::{Backends, DeviceDescriptor, Limits};
 use shape_builder::{ShapeBuilder, ShapeBuilderFactory, ShapePath};
+
+trait DrawSelf {
+    fn draw_self(&self, draw: &Draw, scale: f32, rotation: f32);
+
+    fn born_time(&self) -> u64 {
+        0
+    }
+}
 
 pub struct Flower {
     pub start: Vec2,
@@ -16,12 +24,51 @@ pub struct Flower {
     pub born_time: u64,
 }
 
-pub struct Model {
+impl DrawSelf for Flower {
+    fn draw_self(&self, draw: &Draw, scale: f32, rotation: f32) {
+        let color = IntoLinSrgba::into_lin_srgba(self.color);
+
+        draw_simple_flower(
+            &draw,
+            self.leaf_count,
+            scale,
+            rotation,
+            self.start,
+            self.tip,
+            self.middle,
+            self.thickness,
+            color,
+        );
+    }
+
+    fn born_time(&self) -> u64 {
+        self.born_time
+    }
+}
+
+pub struct Circle {
+    pub center: Vec2,
+    pub radius: f32,
+    pub color: Alpha<Hsl, f32>,
+}
+impl DrawSelf for Circle {
+    fn draw_self(&self, draw: &Draw, scale: f32, _: f32) {
+        draw.ellipse()
+            .xy(self.center)
+            .radius(self.radius * scale)
+            .color(self.color)
+            .finish();
+    }
+}
+
+struct Model {
     pub height: f32,
     pub width: f32,
     pub mouse_position: Option<Point2>,
     // other params
-    pub flowers: Vec<Flower>,
+    flowers: Vec<Box<dyn DrawSelf>>,
+    pub last_flower_born_time: Option<u64>,
+    pub pressed: bool,
 }
 
 pub async fn run_app() {
@@ -73,6 +120,8 @@ fn model(app: &App) -> Model {
         width: window.w(),
         mouse_position: None,
         flowers: vec![],
+        last_flower_born_time: None,
+        pressed: false,
     }
 }
 
@@ -81,57 +130,16 @@ fn update(_app: &App, model: &mut Model, _update: Update) {}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    let t = app.elapsed_frames().to_f32().unwrap() * 0.1;
 
-    frame.clear(BLACK);
-    // let rotation = PI * 0.01 * t;
-    // let scale = 1.0 + t * 0.01;
-
-    // draw_simple_flower(
-    //     &draw,
-    //     10,
-    //     scale,
-    //     rotation + PI * 0.1,
-    //     vec2(0.0, 0.0),
-    //     vec2(0.0, 80.0),
-    //     0.99,
-    //     30.0,
-    //     YELLOWGREEN,
-    // );
-    // draw_duo_colored_flower(
-    //     &draw,
-    //     10,
-    //     scale * 1.05,
-    //     rotation,
-    //     vec2(0.0, 0.0),
-    //     vec2(0.0, 100.0),
-    //     0.85,
-    //     50.0,
-    //     &[RED, BLUE],
-    // );
-    // draw.ellipse()
-    //     .color(YELLOW)
-    //     .w_h(30.0 * scale, 30.0 * scale)
-    //     .finish();
-
+    frame.clear(WHITESMOKE);
+    
     for f in model.flowers.iter() {
-        let t = (app.elapsed_frames() - f.born_time).to_f32().unwrap() * 0.1;
+        let t = (app.elapsed_frames() - f.born_time()).to_f32().unwrap() * 0.1;
 
-        frame.clear(BLACK);
-        let rotation = PI * 0.005 * t;
+        let rotation = PI * 0.015 * t;
         let scale = 1.0 + t * 0.01;
-        let color = IntoLinSrgba::into_lin_srgba(f.color);
-        draw_simple_flower(
-            &draw,
-            f.leaf_count,
-            scale,
-            rotation,
-            f.start,
-            f.tip,
-            f.middle,
-            f.thickness,
-            color,
-        );
+
+        f.draw_self(&draw, scale, rotation);
     }
 
     // put everything on the frame
@@ -150,16 +158,19 @@ fn draw_simple_flower(
     color: impl IntoLinSrgba<ColorScalar>,
 ) {
     let color = color.into_lin_srgba();
-    let m = (start + tip) * middle;
-    let m1 = m + vec2(thickness * 0.5, 0.0);
-    let m2 = m + vec2(thickness * -0.5, 0.0);
+    let tip_translated = tip - start;
+    let m = (tip_translated) * middle;
+    let m1 = m + vec2(0.0, thickness * 0.5);
+    let m2 = m + vec2(0.0, thickness * -0.5);
+    let root = vec2(0.0, 0.0);
+    let draw = draw.xy(start);
     for i in 0..leaf_count {
         draw.path()
-            .start_shape(start)
-            .add_bezier_curve(vec![m1 * scale, tip * scale])
-            .add_bezier_curve(vec![start])
-            .add_bezier_curve(vec![m2 * scale, tip * scale])
-            .add_bezier_curve(vec![start])
+            .start_shape(root)
+            .add_bezier_curve(vec![m1 * scale, tip_translated * scale])
+            .add_bezier_curve(vec![root])
+            .add_bezier_curve(vec![m2 * scale, tip_translated * scale])
+            .add_bezier_curve(vec![root])
             .as_fill()
             .rotate(i.to_f32().unwrap() * TAU / (leaf_count.to_f32().unwrap()) + rotation)
             .color(color)
@@ -204,20 +215,48 @@ fn draw_duo_colored_flower(
 
 /// Event handler
 fn event(_app: &App, model: &mut Model, event: WindowEvent) {
+    let t = _app.elapsed_frames();
     match event {
-        MouseMoved(pos) => model.mouse_position = Some(pos),
-        MousePressed(_) => {
-            if let Some(pos) = model.mouse_position {
-                model.flowers.push(Flower {
+        MouseMoved(pos) => {
+            model.mouse_position = Some(pos);
+            if model.pressed && model.last_flower_born_time.map_or(true, |b| b + 12 < t) {
+                let tip = pos + vec2(10.0 * random_range(3.0, 8.0), 0.0);
+                let flower = Flower {
                     start: pos,
-                    tip: pos + vec2(10.0 * random_range(1.0, 4.0), 0.0),
+                    tip: tip,
                     middle: random_range(0.0, 1.0),
-                    thickness: random_range(10.0, 50.0),
-                    color: hsla(random_range(0.0, 360.0), random_range(0.4, 0.7), 0.56, 0.75),
+                    thickness: random_range(10.0, 40.0),
+                    color: hsla(
+                        random_range(0.0, 360.0),
+                        random_range(0.4, 0.7),
+                        random_range(0.2, 0.5),
+                        0.85,
+                    ),
                     leaf_count: random_range(3, 10) * 2,
-                    born_time: _app.elapsed_frames(),
-                });
+                    born_time: t,
+                };
+                model.flowers.push(Box::new(flower));
+                if random::<bool>() {
+                    let circle = Circle {
+                        center: pos,
+                        radius: pos.distance(tip) * 0.15,
+                        color:hsla(
+                            random_range(0.0, 360.0),
+                            random_range(0.4, 0.7),
+                            random_range(0.3, 0.5),
+                            0.95,
+                        )
+                    };
+                    model.flowers.push(Box::new(circle));
+                }
+                model.last_flower_born_time = Some(t);
             }
+        }
+        MousePressed(_) => {
+            model.pressed = true;
+        }
+        MouseReleased(_) => {
+            model.pressed = false;
         }
         _ => (),
     }
